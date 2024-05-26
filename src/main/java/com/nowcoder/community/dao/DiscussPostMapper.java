05ea8fe6db60f243191cc3578e4b55ff04395e01
bosunrunner <bosunrunner@gmail.com>
2024-05-26 15:55:09 +0800

    首次提交

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,21 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    List<DiscussPost> selectDiscussPosts(int userId,int offset,int limit);
+
+    // 动态添加条件，并且有且仅有一个参数，必须加上参数别名注解 @Param
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+
+
+
+
+}
