2c5f70c2323fdeac344cac3ff81466f13635104d
bosunrunner <bosunrunner@gmail.com>
2024-06-16 21:26:04 +0800

    [Feature] 显示登录信息：使用拦截器interceptor处理每个请求

---
+++ src/main/java/com/nowcoder/community/controller/interceptor/AlphaInterceptor.java
@@ -0,0 +1,34 @@
+package com.nowcoder.community.controller.interceptor;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class AlphaInterceptor implements HandlerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(AlphaInterceptor.class);
+
+    // 在Controller之前执行
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.debug("preHandler:" + handler.toString());
+        return true;
+    }
+
+    // 调用完Controller之后执行
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.debug("postHandler: " + handler.toString());
+    }
+
+    // 在模板引擎TempleEngine执行之后执行
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        logger.debug("afterCompletion:" + handler.toString());
+    }
+}
